b9bb0dfabae7b0568dd92294760e4712f99c3bef
John Hayes <johnnyhayes7@gmail.com>
2019-05-12 02:36:09 +0000

    Automatically backed up by Learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -41,7 +41,7 @@ class PetsController < ApplicationController
      if !params["owner"]["name"].empty?
         @pet.owner == Owner.create(name: params["owner"]["name"])
       end 
-    
+    @pet.save
   # binding.pry
     redirect to "pets/#{@pet.id}"
   end
